2042143500466bc40fb84d1e1a77d8c96aca241e
Mary Ayobami Ogunmola <ogunmolamaryayobami@gmail.com>
2026-05-13 13:52:56 -0600

    Change hardware type from 'd7615' to 'c6620'

--- profile.py
+++ profile.py
@@ -12,7 +12,7 @@ pc = portal.Context()
 request = pc.makeRequestRSpec()
 
 node_0 = request.RawPC('node-0')
-node_0.hardware_type = 'd7615'
+node_0.hardware_type = 'c6620'
 node_0.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU20-64-STD'
 
 # Install and execute a script that is contained in the repository.
