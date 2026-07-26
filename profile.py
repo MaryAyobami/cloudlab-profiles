2e21b957dc24f48e89c38a61b36ab27caa1b4172
Mary Ayobami Ogunmola <ogunmolamaryayobami@gmail.com>
2026-07-25 19:28:01 -0600

    Define multiple node types for RSpec request
    Added support for multiple node types in the request RSpec.

--- profile.py
+++ profile.py
@@ -8,11 +8,40 @@ import geni.rspec.pg as pg
 # Create a portal context.
 pc = portal.Context()
 
-# Create a Request object to start building the RSpec.
+node_types = [
+    ("c220g2", "c220g2)"),
+    ("c220g5", "c220g5)"),
+    ("sm110p", "sm110p"),
+    ("sm220u", "sm220u"),
+    ("r650", "r650"),
+    ("c6620", "c6620"),
+    ("d760", "d760"),
+    ("c6525-25g", "c6525-25g"),
+    ("c6525-100g", "c6525-100g"),
+    ("r6525", "r6525"),
+    ("r6615", "r6615"),
+    ("d7615", "d7615"),
+]
+
+
+pc.defineParameter(
+    "nodeType",
+    "Node type",
+    portal.ParameterType.STRING,
+    node_types[0][0],
+    node_types,
+    "",
+)
+
+
+
+
+params = pc.bindParameters()
+
 request = pc.makeRequestRSpec()
 
 node_0 = request.RawPC('node-0')
-node_0.hardware_type = 'c220g2'
+node_0.hardware_type = params.nodeType
 node_0.disk_image = 'urn:publicid:IDN+wisc.cloudlab.us+image+redshift-PG0:ksplit-eval-test-nd'
 
 # Install and execute a script that is contained in the repository.
